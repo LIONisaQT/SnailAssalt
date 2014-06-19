ed3299fa600c4bcd91d806b220db579ab19e0a8b
LIONisaQT <ryan.shee@gmail.com>
2014-06-19 11:43:23 -0700

    Cleaner button code allows for easier button addition; game state code reflects

--- core/src/com/missionbit/snailassalt/SnailAssalt.java
+++ core/src/com/missionbit/snailassalt/SnailAssalt.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import java.util.ArrayList;
 
 public class SnailAssalt extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -15,11 +16,12 @@ public class SnailAssalt extends ApplicationAdapter {
     private int width;
     private int height;
     private Vector3 temp;
+    private Texture background;
     private BitmapFont font;
     //buttons start
-    private StartButton startButton;
-    private ShopButton shopButton;
-    private BackButton backButton;
+    private StartButton startButtonMenu;
+    private ShopButton shopButtonMenu;
+    private BackButton backButtonShop, backButtonGameOver, backButtonLevelSelect; //different back buttons because their position will most likely be different
     private LevelSelectButton levelSelectButton;
     private LoseButton loseButton;
     //buttons end
@@ -33,8 +35,12 @@ public class SnailAssalt extends ApplicationAdapter {
     private Enemy people;
     private Enemy boss;
     //enemies end
-    //game states
+    // game states -- 1 int to hold current game state, 5 ints to hold MAIN game states
     private int gameState, stateMainMenu, stateInGame, stateGameOver, stateShop, stateLevelSelect;
+    //levels -- 1 String to hold current level, [int] Strings to hold levels
+    private ArrayList<Levels> levels;
+    private int currentLevel;
+    //private String currentLevel, level1, level2;
 	@Override
     public void render () {
         Gdx.gl.glClearColor(1, 1, 1, 1);
@@ -48,6 +54,7 @@ public class SnailAssalt extends ApplicationAdapter {
         height = Gdx.graphics.getHeight();
         camera = new OrthographicCamera(width, height);
         font = new BitmapFont(Gdx.files.internal("font.fnt"), Gdx.files.internal("font.png"), false);
+        background = new Texture("sidewaysmenu.png");
         //game states start
         stateMainMenu = 0;
         stateInGame = 1;
@@ -56,23 +63,23 @@ public class SnailAssalt extends ApplicationAdapter {
         stateLevelSelect = 4;
         gameState = stateMainMenu;
         //game states end
+        //levels start
+        //nothing here yet because coding is too hard :c
+        //levels end
         temp = new Vector3();
         //buttons start
-        startButton = new StartButton(10, 10);
-        shopButton = new ShopButton(10, startButton.buttonGetHeight() + 20);
-        backButton = new BackButton(width - 210, 10);
+        startButtonMenu = new StartButton(10, 10);
+        shopButtonMenu = new ShopButton(10, startButtonMenu.buttonGetHeight() + 20);
+        backButtonGameOver = new BackButton(width - 210, 10);
+        backButtonShop = new BackButton(width - 210, 10);
+        backButtonLevelSelect = new BackButton(width - 210, 10);
         levelSelectButton = new LevelSelectButton(10, height - 410);
         loseButton = new LoseButton(width - 210, height - 210);
         //buttons end
         jimmy = new Player();
         //enemies start
         standardSnail = new Enemy();
-        acidSnail = new Enemy();
-        flyingSnail=new Enemy();
-        healerSnail = new Enemy();
-        motherSnail=new Enemy();
-        people=new Enemy();
-        boss=new Enemy();
+        //pull vivian's enemies later
         //enemies end
         resetGame();
     }
@@ -80,34 +87,16 @@ public class SnailAssalt extends ApplicationAdapter {
         camera.position.set(width/2, height/2, 0);
         gameState = stateMainMenu;
         //buttons start
-        startButton.buttonPosition.set(startButton.getXPos(), startButton.getYPos());
-        shopButton.buttonPosition.set(shopButton.getXPos(), shopButton.getYPos());
-        backButton.buttonPosition.set(backButton.getXPos(), backButton.getYPos());
+        startButtonMenu.buttonPosition.set(startButtonMenu.getXPos(), startButtonMenu.getYPos());
+        shopButtonMenu.buttonPosition.set(shopButtonMenu.getXPos(), shopButtonMenu.getYPos());
+        backButtonGameOver.buttonPosition.set(backButtonGameOver.getXPos(), backButtonGameOver.getYPos());
+        backButtonLevelSelect.buttonPosition.set(backButtonLevelSelect.getXPos(), backButtonLevelSelect.getYPos());
+        backButtonShop.buttonPosition.set(backButtonShop.getXPos(), backButtonShop.getYPos());
         levelSelectButton.buttonPosition.set(levelSelectButton.getXPos(), levelSelectButton.getYPos());
         loseButton.buttonPosition.set(loseButton.getXPos(), loseButton.getYPos());
         //buttons end
         //enemies start
-        standardSnail.standardSnailBound.x = 400;
-        standardSnail.standardSnailBound.y = 400;
-        standardSnail.hp=20;
-        acidSnail.acidSnailBound.x = 0;
-        acidSnail.acidSnailBound.y = 0;
-        acidSnail.hp=60;
-        flyingSnail.flyingSnailBound.x = 0;
-        flyingSnail.flyingSnailBound.y = 0;
-        flyingSnail.hp=20;
-        healerSnail.healerSnailBound.x = 0;
-        healerSnail.healerSnailBound.y = 0;
-        healerSnail.hp=40;
-        motherSnail.motherSnailBound.x = 0;
-        motherSnail.motherSnailBound.y = 0;
-        motherSnail.hp=60;
-        people.peopleBound.x = 0;
-        people.peopleBound.y = 0;
-        people.hp=40;
-        boss.bossBound.x = 0;
-        boss.bossBound.y = 0;
-        boss.hp=100;
+        //pull vivian's enemy stats later
         //enemies end
     }
     public Vector3 getTapPosition() {
@@ -116,24 +105,22 @@ public class SnailAssalt extends ApplicationAdapter {
     }
     public void updateGame(){
         if (gameState == stateMainMenu) {
-            if (Gdx.input.justTouched() && startButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
+            if (Gdx.input.justTouched() && startButtonMenu.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateLevelSelect;
-            if (Gdx.input.justTouched() && shopButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
+            if (Gdx.input.justTouched() && shopButtonMenu.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateShop;
         }
         else if (gameState == stateLevelSelect) {
             if (Gdx.input.justTouched() && levelSelectButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateInGame;
-            if (Gdx.input.justTouched() && backButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
+            if (Gdx.input.justTouched() && backButtonLevelSelect.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateMainMenu;
         }
         else if (gameState == stateShop) {
-            if (Gdx.input.justTouched() && backButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
+            if (Gdx.input.justTouched() && backButtonShop.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateMainMenu;
         }
         else if (gameState == stateInGame) {
-            if (Gdx.input.justTouched() && backButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
-                gameState = stateMainMenu;
             if (Gdx.input.justTouched() && loseButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateGameOver;
             if (standardSnail.standardSnailBound.contains(Gdx.input.getX(), Gdx.input.getY()))
@@ -141,7 +128,7 @@ public class SnailAssalt extends ApplicationAdapter {
             //some code here to determine loss condition
         }
         else if (gameState == stateGameOver) {
-            if (Gdx.input.justTouched() && backButton.buttonBound.contains(getTapPosition().x, getTapPosition().y))
+            if (Gdx.input.justTouched() && backButtonGameOver.buttonBound.contains(getTapPosition().x, getTapPosition().y))
                 gameState = stateMainMenu;
         }
     }
@@ -152,21 +139,21 @@ public class SnailAssalt extends ApplicationAdapter {
         font.setScale(2);
         font.setColor(0, 0, 0, 1);
         if (gameState == stateMainMenu) {
-            batch.draw(startButton.buttonImage, startButton.buttonPosition.x, startButton.buttonPosition.y);
-            batch.draw(shopButton.buttonImage, shopButton.buttonPosition.x, shopButton.buttonPosition.y);
+            batch.draw(background, 0, 0);
+            batch.draw(startButtonMenu.buttonImage, startButtonMenu.buttonPosition.x, startButtonMenu.buttonPosition.y);
+            batch.draw(shopButtonMenu.buttonImage, shopButtonMenu.buttonPosition.x, shopButtonMenu.buttonPosition.y);
             font.draw(batch, "Current state: main menu", 10, height - 50);
         }
         else if (gameState == stateLevelSelect) {
             batch.draw(levelSelectButton.buttonImage, levelSelectButton.buttonPosition.x, levelSelectButton.buttonPosition.y);
-            batch.draw(backButton.buttonImage, backButton.buttonPosition.x, backButton.buttonPosition.y);
+            batch.draw(backButtonLevelSelect.buttonImage, backButtonLevelSelect.buttonPosition.x, backButtonLevelSelect.buttonPosition.y);
             font.draw(batch, "Current state: level select", 10, height - 50);
         }
         else if (gameState == stateShop) {
-            batch.draw(backButton.buttonImage, backButton.buttonPosition.x, backButton.buttonPosition.y);
+            batch.draw(backButtonShop.buttonImage, backButtonShop.buttonPosition.x, backButtonShop.buttonPosition.y);
             font.draw(batch, "Current state: shop", 10, height - 50);
         }
         else if (gameState == stateInGame) {
-            batch.draw(backButton.buttonImage, backButton.buttonPosition.x, backButton.buttonPosition.y);
             batch.draw(loseButton.buttonImage, loseButton.buttonPosition.x, loseButton.buttonPosition.y);
             batch.draw(jimmy.jimmy, 0, 0);
             batch.draw(standardSnail.standardSnail, standardSnail.standardSnailBound.x, standardSnail.standardSnailBound.y);
@@ -176,10 +163,11 @@ public class SnailAssalt extends ApplicationAdapter {
             font.draw(batch, "Current state: in-game", 10, height - 50);
         }
         else if (gameState == stateGameOver) {
-            batch.draw(backButton.buttonImage, backButton.buttonPosition.x, backButton.buttonPosition.y);
+            batch.draw(backButtonGameOver.buttonImage, backButtonGameOver.buttonPosition.x, backButtonGameOver.buttonPosition.y);
             font.draw(batch, "Game Over", 10, 50);
             font.draw(batch, "Current state: game over", 10, height - 50);
         }
+        font.draw(batch, "Resolution: " + width + ", " + height, 10, height);
         batch.end();
     }
 }
