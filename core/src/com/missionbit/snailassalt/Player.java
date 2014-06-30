a01820c36763742c806438acee426b21699e9a7a
douglas <dmejiaamador@gmail.com>
2014-06-30 11:01:50 -0700

    add images

---
+++ core/src/com/missionbit/snailassalt/Player.java
@@ -0,0 +1,33 @@
+package com.missionbit.snailassalt;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Created by douglas on 6/23/14.
+ */
+public class Player {
+        public Texture texture;
+        public Sprite sprite;
+        public Rectangle bound;
+        public int currentWaterGun, squirtGun, superSoaker;
+        public int curency;
+        public Player () {
+            texture = new Texture("jimmy.png");
+            sprite = new Sprite(texture,60,60, texture.getWidth(), texture.getHeight());
+            bound = new Rectangle(60,60, sprite.getWidth(), sprite.getHeight());
+            curency = 0;
+            squirtGun = 0;
+            superSoaker = 1;
+            currentWaterGun = squirtGun;
+        }
+        public void playerPosition(float c,float d){
+            bound.setX(c);
+            bound.setY(d);
+            sprite.setX(c);
+            sprite.setY(d);
+        }
+    }
+
+
