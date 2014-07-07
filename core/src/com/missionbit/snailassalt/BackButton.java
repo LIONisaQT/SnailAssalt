fb2a1692721aa5e90044e142c0dff3a4cfebb054
vivianlam <vivianlam10497@gmail.com>
2014-07-07 10:58:08 -0700

    sgjhfdjkghfjskgjkshgkjsfghkjshgkjsfh

--- core/src/com/missionbit/snailassalt/BackButton.java
+++ core/src/com/missionbit/snailassalt/BackButton.java
@@ -3,7 +3,7 @@ package com.missionbit.snailassalt;
   Created by ryansheeisaqt on 6/18/14.
  */
 public class BackButton extends Button {
-    public BackButton(float x, float y) {super(x, y, "backButton.png", "backButtonNope.png");}
+    public BackButton(float x, float y) {super(x, y, "back button.png", "backButtonNope.png");}
     public void pressedAction() {
         SnailAssalt.gameState = SnailAssalt.GameState.MAINMENU;
     }
