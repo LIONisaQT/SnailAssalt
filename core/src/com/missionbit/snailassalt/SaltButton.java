b5d5891cdb1413d38b27ce3c2adf7f18bdfe515f
douglas <dmejiaamador@gmail.com>
2014-07-09 11:38:12 -0700

    preference works and so do shop buttons

--- core/src/com/missionbit/snailassalt/SaltButton.java
+++ core/src/com/missionbit/snailassalt/SaltButton.java
@@ -5,7 +5,7 @@ package com.missionbit.snailassalt;
  */
 public class SaltButton extends Button {
     public SaltButton(float x ,float y){
-            super(x, y,"shopButton.png");
+            super(x, y,"Salt.png");
         }
     }
 
