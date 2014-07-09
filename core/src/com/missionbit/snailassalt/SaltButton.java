3409287bedd55dcabc37d8a833de08cbd64d6d9c
douglas <dmejiaamador@gmail.com>
2014-07-09 14:07:35 -0700

    merged much_waves with master

--- core/src/com/missionbit/snailassalt/SaltButton.java
+++ core/src/com/missionbit/snailassalt/SaltButton.java
@@ -5,7 +5,7 @@ package com.missionbit.snailassalt;
  */
 public class SaltButton extends Button {
     public SaltButton(float x ,float y){
-            super(x, y,"Salt.png");
+            super(x, y,"Salt.png","Salt.png");
         }
     }
 
