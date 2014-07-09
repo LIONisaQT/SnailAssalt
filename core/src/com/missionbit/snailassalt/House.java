47b8bd537c9354eab9d8a2728e18a780e29108de
LIONisaQT <ryan.shee@gmail.com>
2014-07-09 10:44:38 -0700

    House now correctly displays 3 states

--- core/src/com/missionbit/snailassalt/House.java
+++ core/src/com/missionbit/snailassalt/House.java
@@ -20,10 +20,10 @@ public class House {
         houseFull = new Sprite(house1, house1.getWidth(), (int)height);
         houseFull.setPosition(width - house1.getWidth(), 0);
         house2 = new Texture("balconyz.png");
-        houseHalf = new Sprite(house2, house1.getWidth(), (int)height);
+        houseHalf = new Sprite(house2, house2.getWidth(), (int)height);
         houseHalf.setPosition(width - house2.getWidth(), 0);
         house3 = new Texture("balconya.png");
-        houseZero = new Sprite(house3, house1.getWidth(), (int)height);
+        houseZero = new Sprite(house3, house3.getWidth(), (int)height);
         houseZero.setPosition(width - house3.getWidth(), 0);
         Housebounds = new Rectangle(width - houseFull.getWidth(), 0, houseFull.getWidth(), houseFull.getHeight());
         maxHP = 200.0f;
@@ -32,7 +32,7 @@ public class House {
     }
     public void draw(SpriteBatch batch){
         if (hp >= 2 * maxHP / 3) {houseFull.draw(batch);}
-        else if (hp < 2 * maxHP / 3) {houseHalf.draw(batch);}
+        else if (hp < 2 * maxHP / 3 && hp > maxHP / 3) {houseHalf.draw(batch);}
         else if (hp <= maxHP / 3) {houseZero.draw(batch);}
     }
 
