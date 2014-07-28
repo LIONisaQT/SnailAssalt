f64cb84ee5e5dceefece3d42e1841e6e9919eb4a
douglas <dmejiaamador@gmail.com>
2014-07-28 13:51:06 -0700

    merge with tutorialMoves branch shit werks

--- core/src/com/missionbit/snailassalt/BackButton.java
+++ core/src/com/missionbit/snailassalt/BackButton.java
@@ -8,11 +8,22 @@ public class BackButton extends Button {
     }
     public void pressedAction() {
         if (SnailAssalt.prevGameState == SnailAssalt.GameState.GAMEOVER) {SnailAssalt.gameState = SnailAssalt.prevGameState;}
-        else if (SnailAssalt.prevGameState == SnailAssalt.GameState.WIN) {SnailAssalt.gameState = SnailAssalt.prevGameState;}
+        else if (SnailAssalt.prevGameState == SnailAssalt.GameState.WIN) {SnailAssalt.gameState = SnailAssalt.prevGameState;} else if (SnailAssalt.gameState == SnailAssalt.gameState.TUTORIAL) {
+            if (SnailAssalt.tutState == SnailAssalt.TutorialState.PAGE4) {
+                SnailAssalt.preferences.putInteger("tutorial", 1);
+            }
+            if (SnailAssalt.preferences.getInteger("tutorial", 1) == 1) {
+                SnailAssalt.gameState = SnailAssalt.GameState.LEVELSELECT;
+
+            }
+        }
+
         else {
             SnailAssalt.gameState = SnailAssalt.GameState.MAINMENU;
             SnailAssalt.tutState = SnailAssalt.TutorialState.PAGE1;
         }
+
         SnailAssalt.prevGameState = null; //clear prevGameState
+
     }
 }
